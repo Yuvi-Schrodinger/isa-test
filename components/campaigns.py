90dacce1d3547bb71ac9e40a6fad04efdc824021
Yuvi <adm.yuvi@gmail.com>
2024-05-21 11:59:31 -0300

    Update campaigns.py

--- components/campaigns.py
+++ components/campaigns.py
@@ -19,20 +19,24 @@ ad_acc = "956757482423549"
 
 fb_api = GraphAPI(ad_acc, fb_api)
 
-campaign_insights = pd.DataFrame(fb_api.get_insights(ad_acc)["data"])
-adset_status = pd.DataFrame(fb_api.get_adset_status(ad_acc)["data"])
-adset_insights = pd.DataFrame(fb_api.get_insights(ad_acc, "adset")["data"])
-ads_insights = pd.DataFrame(fb_api.get_insights(ad_acc, "ad")["data"])
+campaign_insights = pd.DataFrame(fb_api.get_insights())
+adset_insights = pd.DataFrame(fb_api.get_insights(level="adset"))  # Corrigido: nível de detalhe para "adset"
+campaign_status = pd.DataFrame(fb_api.get_campaigns_status())
+
+
+adset_insights.head(3)
+# data_over_time = fb_api.get_data_over_time(23849930731190625)
+data_over_time = fb_api.get_data_over_time(120208363974670077)
 
 
 # =========  Layout  =========== #
 layout = html.Div([
             dbc.Row([
-                html.H3("Selecione o conjunto de anúncio:", style={"margin-top": "50px"}),
+                html.H3("Selecione a campanha:", style={"margin-top": "50px"}),
                 dcc.Dropdown(
-                    options=[{"label": i, "value": i} for i in adset_insights.adset_name.values],
-                    value=adset_insights.adset_name.values[0],
-                    id='dd-adset'),
+                    options=[{"label": i, "value": i} for i in campaign_insights.campaign_name.values],
+                    value=campaign_insights.campaign_name.values[0],
+                    id='dd-campaign'),
                 ], style={"margin-bottom": "30px"}),
 
             dbc.Row([
@@ -40,8 +44,7 @@ layout = html.Div([
                         dbc.Card([
                             dbc.CardHeader("Status"),
                             dbc.CardBody([
-                                dbc.Button("", id="btn-adset-status"),
-                            ], id="cb-status-adset")
+                            ], id="cb-status")
                         ], color="light"),
 
                     ], md=2),
@@ -50,7 +53,7 @@ layout = html.Div([
                     dbc.Card([
                             dbc.CardHeader("Clicks"),
                             dbc.CardBody([
-                                html.H4("", id="adset-clicks", style={"color": "var(--bs-info)"}),
+                                html.H4("", id="campaign-clicks", style={"color": "var(--bs-info)"}),
                             ])
                         ], color="light"),
 
@@ -60,7 +63,7 @@ layout = html.Div([
                     dbc.Card([
                             dbc.CardHeader("Spend"),
                             dbc.CardBody([
-                                html.H4("", id="adset-spend", style={"color": "var(--bs-primary)"}),
+                                html.H4("", id="campaign-spend", style={"color": "var(--bs-primary)"}),
                             ])
                         ], color="light"),
                     ], md=2),
@@ -69,7 +72,7 @@ layout = html.Div([
                     dbc.Card([
                             dbc.CardHeader("Conversion"),
                             dbc.CardBody([
-                                html.H5("", id="adset-conversions", style={"color": "var(--bs-primary)"}),
+                                html.H5("", id="campaign-conversions", style={"color": "var(--bs-primary)"}),
                             ])
                         ], color="light"),
                     ], md=2),
@@ -78,34 +81,32 @@ layout = html.Div([
             dbc.Row([
                 html.H4("Selecione o indicador:"),
                 dcc.RadioItems(options=['Spend', 'CPC', 'CPM', 'Clicks', 'Conversion'], 
-                            value='Conversion', id='adset-kind', 
+                            value='Conversion', id='campaign-kind', 
                             inputStyle={"margin-right": "5px", "margin-left": "20px"}),
                 ], style={"margin-top": "50px"}),
 
             dbc.Row([            
-                dbc.Col(dcc.Graph(id="graph-line-adset"), md=6),
-                dbc.Col(dcc.Graph(id="graph-bar-adset"), md=6)
+                dbc.Col(dcc.Graph(id="graph-line-campaign"), md=6),
+                dbc.Col(dcc.Graph(id="graph-bar-campaign"), md=6)
                 ], style={"margin-top": "20px"}),
             ]) 
 
 #========== Callbacks ================
 @app.callback([
-                Output("cb-status-adset", "children"),
-                Output("adset-clicks", "children"),
-                Output("adset-spend", "children"),
-                Output("adset-conversions", "children"),
+                Output("cb-status", "children"),
+                Output("campaign-clicks", "children"),
+                Output("campaign-spend", "children"),
+                Output("campaign-conversions", "children"),
             ], 
-                [Input("dd-adset", "value"),
+                [Input("dd-campaign", "value"),
                 ])
-def render_page_content(adset):
-    adset=adset_insights.adset_name.values[0]
+def render_page_content(campaign):
+    status = campaign_status[campaign_status["name"] == campaign]["status"].values[0]
+    clicks = campaign_insights[campaign_insights["campaign_name"] == campaign]["clicks"]
+    spend = "R$ " + campaign_insights[campaign_insights["campaign_name"] == campaign]["spend"]
 
-    status = adset_status[adset_status["name"] == adset]["status"].values[0]
-    clicks = adset_insights[adset_insights["adset_name"] == adset]["clicks"]
-    spend = "R$ " + adset_insights[adset_insights["adset_name"] == adset]["spend"]
-
-    adset_id = adset_status[adset_status["name"] == adset]["id"].values[0]
-    data_over_time = fb_api.get_data_over_time(adset_id)
+    campaign_id = campaign_status[campaign_status["name"] == campaign]["id"].values[0]
+    data_over_time = fb_api.get_data_over_time(campaign_id)
     conversions = pd.DataFrame(data_over_time["data"])["conversion"].fillna(0).sum()
 
     if status == "PAUSED":
@@ -115,31 +116,26 @@ def render_page_content(adset):
     return status, clicks, spend, conversions
     
 
-
 @app.callback([
-                Output("graph-line-adset", "figure"),
-                Output("graph-bar-adset", "figure"),
+                Output("graph-line-campaign", "figure"),
+                Output("graph-bar-campaign", "figure"),
             ], 
-                [Input("dd-adset", "value"),
-                Input("adset-kind", "value"),
+                [Input("dd-campaign", "value"),
+                Input("campaign-kind", "value"),
                 Input(ThemeChangerAIO.ids.radio("theme"), "value")]
             )
-def render_page_content(adset, adset_kind, theme):
-    # adset = adset_status["name"].values[0]
-    adset_id = adset_status[adset_status["name"] == adset]["id"].values[0]
-    # adset_kind = "conversion"
-    adset_kind = adset_kind.lower()   
-    
-    data_over_time = fb_api.get_data_over_time(adset_id)
+def render_page_content(campaign, campaign_kind, theme):
+    campaign_kind = campaign_kind.lower()
+
+    campaign_id = campaign_status[campaign_status["name"] == campaign]["id"].values[0]
+    data_over_time = fb_api.get_data_over_time(campaign_id)
     df_data = pd.DataFrame(data_over_time["data"])
     df_data["clicks"] = df_data["clicks"].astype(np.float64)
     
-    fig_line = px.line(df_data, x="date_start", y=adset_kind, template=template_from_url(theme))
+    fig_line = px.line(df_data, x="date_start", y=campaign_kind, template=template_from_url(theme))
     fig_line.update_layout(margin=go.layout.Margin(l=0, r=0, t=0, b=0))
-    
-    df_adset = ads_insights[ads_insights["adset_name"]== adset]
-    fig_adsets = px.bar(df_adset, y=adset_kind, x="ad_name", template=template_from_url(theme))
+
+    df_adset = adset_insights[adset_insights["campaign_name"]== campaign]
+    fig_adsets = px.bar(df_adset, y=campaign_kind, x="adset_id", template=template_from_url(theme))
     fig_adsets.update_layout(margin=go.layout.Margin(l=0, r=0, t=0, b=0))
     return fig_line, fig_adsets
-
-    
