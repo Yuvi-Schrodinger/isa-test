0266ec55e08c3d614ce18a31b64f5e77ea237bea
Yuvi <adm.yuvi@gmail.com>
2024-05-21 11:58:47 -0300

    Update graph_api.py

--- graph_api.py
+++ graph_api.py
@@ -3,67 +3,88 @@ import plotly.express as px
 import requests
 import streamlit as st
 import json
-import plotly.express as px
-#tentar criar classe da api por aqui mesmo
+
+import requests
 
 class GraphAPI:
     def __init__(self, ad_acc, fb_api):
         self.base_url = "https://graph.facebook.com/v19.0/"
-        self.api_fields = ["spend", "cpc", "cpm", "objective", "adset_name", 
-                "adset_id", "clicks", "campaign_name", "campaign_id", 
-                "conversions", "frequency", "conversion_values", "ad_name", "ad_id"]
-        self.token = "&access_token=" + fb_api
+        self.api_fields = [
+            "spend", "cpc", "cpm", "objective", "adset_name", 
+            "adset_id", "clicks", "campaign_name", "campaign_id", 
+            "conversions", "frequency", "conversion_values", "ad_name", "ad_id"
+        ]
+        self.token = fb_api
         self.ad_acc = ad_acc
 
-    def get_insights(self, ad_acc, level="campaign"):
-        url = self.base_url + "act_" + str(ad_acc)
-        url += "/insights?level=" + level
-        url += "&fields=" + ",".join(self.api_fields)
+    def make_request(self, endpoint, params=None):
+        url = self.base_url + endpoint
+        if params is None:
+            params = {}
+        params["access_token"] = self.token
 
-        data = requests.get(url + self.token)
-        data = json.loads(data._content.decode("utf-8"))
-        for i in data["data"]:
-            if "conversions" in i:
-                i["conversion"] = float(i["conversions"][0]["value"])
-        return data
+        response = requests.get(url, params=params)
+        try:
+            response.raise_for_status()
+            return response.json()
+        except requests.HTTPError as e:
+            print(f"Error fetching data: {e}")
+            return None
 
-# act_3120164588217844/insights?level=adset&fields=spend,cpc,cpm,objective,adset_name,adset_id,clicks,campaign_name,campaign_id,conversions,frequency,conversion_values,ad_name,ad_id,ad_impression_actions
+    def get_insights(self, level="campaign"):
+        endpoint = f"act_{self.ad_acc}/insights"
+        params = {
+            "fields": ",".join(self.api_fields),
+            "level": level
+        }
+        return self.make_request(endpoint, params)
 
-    def get_campaigns_status(self, ad_acc):
-        url = self.base_url + "act_" + str(ad_acc)
-        url += "/campaigns?fields=name,status,adsets{name, id}"
-        data = requests.get(url + self.token)
-        return json.loads(data._content.decode("utf-8"))
+    def get_campaigns_status(self):
+        endpoint = f"act_{self.ad_acc}/campaigns"
+        params = {
+            "fields": "name,status,id"
+        }
+        return self.make_request(endpoint, params)
 
-    def get_adset_status(self, ad_acc):
-        url = self.base_url + "act_" + str(ad_acc)
-        url += "/adsets?fields=name,status,id"
-        data = requests.get(url + self.token)
-        return json.loads(data._content.decode("utf-8"))
+    def get_adset_status(self):
+        endpoint = f"act_{self.ad_acc}/adsets"
+        params = {
+            "fields": "name,status,id"
+        }
+        return self.make_request(endpoint, params)
 
-    def get_data_over_time(self, campaign):
-        url = self.base_url + str(campaign)
-        url += "/insights?fields="+ ",".join(self.api_fields)
-        url += "&date_preset=last_30d&time_increment=1"
-
-        data = requests.get(url + self.token)
-        data = json.loads(data._content.decode("utf-8"))
-        for i in data["data"]:
-            if "conversions" in i:
-                i["conversion"] = float(i["conversions"][0]["value"])
-        return data
+    def get_data_over_time(self, campaign_id):
+        endpoint = f"{campaign_id}/insights"
+        params = {
+            "fields": ",".join(self.api_fields),
+            "date_preset": "last_30d",
+            "time_increment": "1"
+        }
+        return self.make_request(endpoint, params)
 
 
-# act_3120164588217844/campaigns?fields=name,status, adsets
+
 if __name__ == "__main__":
     fb_api = open("tokens/token").read()
     ad_acc = "956757482423549"
 
-    self = GraphAPI(ad_acc, fb_api)
+    fb_graph_api = GraphAPI(ad_acc, fb_api)
 
-    self.get_insights(ad_acc)
-    self.get_campaigns_status(ad_acc)["data"]
+    st.title("Dashboard da Isa")
 
-    self.get_data_over_time(120208363974670077)
-    self.get_data_over_time(120208363974670077)
+    st.header("Campaign status")
+    campaign_status = fb_graph_api.get_campaigns_status()
+    st.write(campaign_status)
 
+    st.header("Adset Status")
+    adset_status = fb_graph_api.get_adset_status()
+    st.write(adset_status)
+
+    st.header("Insights")
+    insights = fb_graph_api.get_insights()
+    st.write(insights)
+
+    st.header("Data Over Time for Campaign")
+    campaign_id = 120208363974670077
+    data_over_time = fb_graph_api.get_data_over_time(campaign_id)
+    st.write(data_over_time)
